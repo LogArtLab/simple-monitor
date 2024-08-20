1da8e43471246baeeee5a78a5d5ffeda86925ea4
Simone Silvetti <simone.silvetti@gmail.com>
2024-08-20 16:04:27 +0200

    added comment to examplepy

--- example.py
+++ example.py
@@ -1,20 +1,17 @@
 import numpy as np
-from antlr4 import InputStream, CommonTokenStream
 
 from stl.Monitor import STLMonitorBuilder
 from stl.RobSemantics import RobSemantics
-from stl.STLLexer import STLLexer
-from stl.STLParser import STLParser
 from stl.TimeSeries import TimeSeries
 
-# formula = '(G_[1,6] (Q>2)) | (!(F_[1,10](P<=4)))'
-timeSeries = TimeSeries(['P', 'Q'],
-                        np.array([0, 1, 2, 3, 4]),
-                        np.array([[5, 67, -6, -1, -1],
-                                  [3, 3, -3, -1, -1]]))
+timeSeries = TimeSeries(['P', 'Q'],  # variables
+                        np.array([0, 1, 2, 3, 4]),  # timesteps
+                        np.array([[5, 67, -6, -1, -1],  # values assumed by the first variable for each timestep
+                                  [3, 3, -3, -1, -1]]))  # values assumed by the first variable for each timestep
 
-formula = '(G_[1,6] (Q>-20))'
+# formula = '(G_[1,6] (Q>2)) | (!(F_[1,10](P<=4)))'
+formula = '(G_[1,6] (Q>-20))'  # STL formula
 monitor = STLMonitorBuilder(formula).build()
-value = monitor.monitor(RobSemantics(timeSeries=timeSeries, currentState=0))
-print(value)
+robustness_value = monitor.monitor(RobSemantics(timeSeries=timeSeries, currentState=0))
+print(robustness_value)
 print("DONE")
